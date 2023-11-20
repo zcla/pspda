276dd2ab02145daacc2ea69c3b56af594e76f1d9
zcla <zeclaudioaguiar@gmail.com>
2023-11-19 22:36:51 -0300

    tratamento de parâmetros

--- site/js/index.js
+++ site/js/index.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-    const params = window.location.search.split(/\?|&/).filter((str) => str).map((str) => str.split('='));
-    const pagina = params.filter((arr) => arr[0] == 'pagina')[0][1];
-    $(`#pagina_${pagina}`).show();
+	const params = window.location.search.split(/\?|&/).filter((str) => str).map((str) => str.split('='));
+	if (params.length > 0) {
+		const pagina = params.filter((arr) => arr[0] == 'pagina')[0][1];
+		$(`#pagina_${pagina}`).show();
+	}
 }

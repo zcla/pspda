a4f7d1707c5208d2e03cd4c9994c8f33380fd16e
zcla <zeclaudioaguiar@gmail.com>
2023-11-28 00:24:30 -0300

    Conteúdo em json

--- site/js/index.js
+++ site/js/index.js
@@ -1,7 +1,52 @@
+function mostraRoteiro(roteiro) {
+	const selector = '#conteudo';
+	$(selector).empty();
+	$(selector).append('<div class="spinner-border">');
+	$.getJSON(`data/roteiro/${roteiro}.json`, function(data) {
+		console.log(data);
+		$(selector).empty();
+		$(selector).append(`
+			<h1>${data.nome}</h1>
+		`);
+		if (data.nota) {
+			$(selector).append(`
+				<div class="alert alert-info">
+					${data.nota}
+				</div>
+			`);
+		}
+		for (const passo of data.passos) {
+			mostraPasso(selector, passo);
+		}
+	});
+}
+
+function mostraPasso(selector, passo) {
+	let html = '???';
+	switch (passo.tipo) {
+		case 'lista':
+			html = '<ul>';
+			for (const item of passo.itens) {
+				html += `<li>${item}</li>`;
+			}
+			html += '</ul>';
+			break;
+		case 'titulo':
+			html = `<h${passo.nivel + 1}>${passo.texto}</h${passo.nivel + 1}>`;
+			break;
+		default:
+			console.log(`mostraPasso(): Tipo "${passo.tipo}" desconhecido.`)
+			break;
+	}
+	$(selector).append(`
+		<div>${html}</div>
+	`);
+}
+
 window.onload = function() {
 	const params = window.location.search.split(/\?|&/).filter((str) => str).map((str) => str.split('='));
 	if (params.length > 0) {
-		const pagina = params.filter((arr) => arr[0] == 'pagina')[0][1];
-		$(`#pagina_${pagina}`).show();
+		const roteiro = params.filter((arr) => arr[0] == 'roteiro')[0][1];
+		mostraRoteiro(roteiro);
 	}
 }
